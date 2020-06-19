5aabd80fb7d8647b0e35ae0e3a5c4a00dcf46b08
SebastianGarces <gsgarces1@gmail.com>
2020-06-18 23:11:40 -0400

    added spotify client credentials setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,39 @@
+require("dotenv").config();
 const express = require("express");
+const request = require("request-promise");
+const cors = require("cors");
 
 const server = express();
 
+server.use(express.json());
+server.use(cors());
+
 server.get("/", (req, res) => {
-    res.status(200).send("<h1>Server running OK</h1>")
-})
+	res.status(200).send("<h1>Server running OK</h1>");
+});
+
+server.get("/api/login", async (req, res) => {
+	const authOptions = {
+		url: "https://accounts.spotify.com/api/token",
+		form: {
+			grant_type: "client_credentials",
+		},
+		headers: {
+			Authorization:
+				"Basic " +
+				Buffer.from(
+					process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET
+				).toString("base64"),
+		},
+		json: true,
+	};
+
+	const { access_token, refresh_token } = await request
+		.post(authOptions)
+		.then(res => res)
+		.catch(err => err);
+
+	res.status(200).json({ access_token });
+});
 
 module.exports = server;
