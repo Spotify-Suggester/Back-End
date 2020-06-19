aacfcb12205cbf13bbd8060c1c21f6c4bb043071
Khoa Tran <jacktran1799@gmaail.com>
2020-06-19 12:30:59 -0500

    finish setting up the database for development

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require("./api/server.js");
 
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
 	console.log(`\n=== Server listening on http://localhost:${PORT} ===\n`);
+	.then(res=>console.log(res))
 });
---
+++ knexfile.js
@@ -0,0 +1,58 @@
+
+const pgConnection = process.env.DATABASE_URL;
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations:{
+      directory: './data/migrations',
+    },
+    seeds:{
+      directory: './data/seeds',
+    },
+    connection: {
+      filename: './data/users.db3'
+    },
+    pool:{
+      afterCreate: (conn,done)=>{
+        conn.run('Pragma foreign_keys= ON',done);
+      }
+    }
+  },
+
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    pool:{
+      afterCreate: (conn,done)=>{
+        conn.run('Pragma foreign_keys= ON',done);
+      }
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds:{
+      directory: './data/seeds',
+    },
+  },
+};
