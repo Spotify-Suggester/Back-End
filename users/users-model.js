51915058de4bcfe06878585955a088a75d94da76
SebastianGarces <gsgarces1@gmail.com>
2020-06-20 03:04:27 -0400

    corrected users-model to use users instead of user

--- users/users-model.js
+++ users/users-model.js
@@ -8,19 +8,19 @@ module.exports = {
 };
 
 async function add(user) {
-	const [id] = await db("user").insert(user, "id");
+	const [id] = await db("users").insert(user, "id");
 
 	return findById(id);
 }
 
 function find() {
-	return db("user").select("id", "username", "department");
+	return db("users").select("id", "username", "department");
 }
 
 function findBy(filter) {
-	return db("user").where(filter);
+	return db("users").where(filter);
 }
 
 function findById(id) {
-	return db("user").where({ id }).first();
+	return db("users").where({ id }).first();
 }
