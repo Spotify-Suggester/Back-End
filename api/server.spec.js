453f336b24a35db6260e07e39645152840018cc5
SebastianGarces <gsgarces1@gmail.com>
2020-06-25 22:20:11 -0400

    added test script on package.json and ran server.js test

---
+++ api/server.spec.js
@@ -0,0 +1,19 @@
+const server = require("./server.js");
+const request = require("supertest");
+
+describe("server.js", () => {
+	test("should be the testing environment", () => {
+		expect(process.env.DB_ENV).toBe("testing");
+	});
+
+	describe("GET /", () => {
+		it("should return 200 OK", async () => {
+			const res = await request(server).get("/");
+			expect(res.status).toBe(200);
+		});
+		it("should be json", async () => {
+			const res = await request(server).get("/");
+			expect(res.type).toBe("text/html");
+		});
+	});
+});
